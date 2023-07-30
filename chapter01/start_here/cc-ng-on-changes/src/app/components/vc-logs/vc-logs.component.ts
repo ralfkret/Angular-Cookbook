171cbef05fc5f2bb162ba0bd47de0b544b9deec3
Ralf Kretzschmar <ralf.kretzschmar@googlemail.com>
2023-07-30 15:16:26 +0200

    solved - no styles

--- chapter01/start_here/cc-ng-on-changes/src/app/components/vc-logs/vc-logs.component.ts
+++ chapter01/start_here/cc-ng-on-changes/src/app/components/vc-logs/vc-logs.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-vc-logs',
   templateUrl: './vc-logs.component.html',
   styleUrls: ['./vc-logs.component.scss']
 })
-export class VcLogsComponent implements OnInit {
+export class VcLogsComponent implements OnChanges {
   @Input() vName;
-  constructor() { }
-
-  ngOnInit(): void {
+  logs: string[] = [];
+  ngOnChanges(changes: SimpleChanges): void {
+    const chng = changes.vName;
+    if (chng) {
+      if (chng.firstChange){
+        this.logs.push(`First version is ${chng.currentValue}`)
+      } else {
+        this.logs.push(`Added version ${chng.currentValue} (from ${chng.previousValue})`)
+      }
+    }
   }
+
 
 }
