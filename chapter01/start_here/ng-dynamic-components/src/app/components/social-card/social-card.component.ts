af68b150c50d2260d4ef17aecdb8f5e45c369c31
Ralf Kretzschmar <Ralf.Kretzschmar@boschrexroth.de>
2023-07-31 14:54:18 +0200

    solved ng-dynamic-components

--- chapter01/start_here/ng-dynamic-components/src/app/components/social-card/social-card.component.ts
+++ chapter01/start_here/ng-dynamic-components/src/app/components/social-card/social-card.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, ComponentFactoryResolver, ViewChild, ViewContainerRef, OnChanges, SimpleChanges, Type } from '@angular/core';
 import { SocialCardType } from 'src/app/constants/social-card-type';
+import { FbCardComponent } from "../fb-card/fb-card.component";
+import { TwitterCardComponent } from "../twitter-card/twitter-card.component";
 
 @Component({
   selector: 'app-social-card',
   templateUrl: './social-card.component.html',
   styleUrls: ['./social-card.component.scss']
 })
-export class SocialCardComponent implements OnInit {
+export class SocialCardComponent implements OnChanges {
   @Input() type: SocialCardType;
-  cardTypes = SocialCardType;
-  constructor() { }
+  @ViewChild("vrf", { read: ViewContainerRef }) vrf: ViewContainerRef;
+  constructor(private cfr: ComponentFactoryResolver) { }
 
-  ngOnInit(): void {
+  private static map = new Map<SocialCardType, Type<unknown>>(
+    [[SocialCardType.Facebook, FbCardComponent],
+    [SocialCardType.Twitter, TwitterCardComponent]]);
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const chgType = changes.type;
+    if (!chgType || chgType.currentValue === undefined || !this.vrf)
+      return;
+    console.log(SocialCardType[chgType.currentValue]);
+    const componentType = SocialCardComponent.map.get(chgType.currentValue);
+    const cf = this.cfr.resolveComponentFactory(componentType);
+    this.vrf.clear();
+    const component = this.vrf.createComponent(cf);
+    console.log(component);
+
   }
 
 }
